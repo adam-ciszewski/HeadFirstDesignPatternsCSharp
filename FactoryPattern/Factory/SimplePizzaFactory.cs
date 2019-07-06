2087ff7de5507a71b8d10c0049ee97d61fa7571d
KarimShavar <deimos.feb@gmail.com>
2019-07-06 16:45:17 +0100

    Factory pattern stub added.

---
+++ FactoryPattern/Factory/SimplePizzaFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryPattern.Entities;
+
+namespace FactoryPattern.Factory
+{
+    public class SimplePizzaFactory
+    {
+        public Pizza CreatePizza(string type)
+        {
+            Pizza pizza = null;
+
+            switch (type)
+            {
+                case "cheese":
+                    pizza = new CheesePizza();
+                    break;
+                case "pepperoni":
+                    pizza = new PepperoniPizza();
+                    break;
+                case "clam":
+                    pizza = new ClamPizza();
+                    break;
+                case "veggie":
+                    pizza = new VeggiePizza();
+                    break;
+                default:
+                    throw new InvalidDataException("Invalid pizza type.");
+
+            }
+
+            return pizza;
+        }
+    }
+}
---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
